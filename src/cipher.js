fdde514d003a9cceded360034e9bd531ff28be1e
RocioChamorro <chamorrorocio5@gmail.com>
2019-02-26 08:27:57 -0500

    Cifrado de una letra

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,59 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+
+const msm = document.getElementById("mensaje");
+const offset = document.getElementById("desplazamiento");
+const boton = document.getElementById("cifrara");
+const msmsalida = document.getElementById("salida");
+boton.addEventListener("click", () => {
+const msmCapturado = msm.value;
+const OffCapturado = offset.value;
+//console.log(msmCapturado)
+//msmsalida.innerHTML = msmCapturado;
+//function encode (msmCapturado, desplazamiento) {
+ 
+  //const desplazamiento = 2;
+  const codigoAssci = msmCapturado.charCodeAt();
+  //console.log(codigoAssci)
+  const nuevoNumeroAssci = (codigoAssci - 65 + OffCapturado) % 26 + 65;
+  const nuevaLetra = String.fromCharCode(nuevoNumeroAssci)
+  console.log(nuevoNumeroAssci)
+  console.log(nuevaLetra) 
+  msmsalida.innerHTML = nuevaLetra;
+  //console.log(mensajeCapturado)
+  
+  });
+
+
+
+
+/*const codigoAssci = msmCapturado.charCodeAt();
+  const nuevoNumeroAssci = (codigoAssci - 65 + desplazamiento) % 26 + 65;
+  const nuevaLetra = String.fromCharCode(nuevoNumeroAssci)
+  //return nuevaLetra;
+  console.log(nuevoNumeroAssci)
+  console.log(nuevaLetra)
+  console.log(msmCapturado)
+  //msmsalida.innerHTML = nuevaLetra;
+
+
+ //}
+ */
+//}); 
+
+
+
+/*
+//window.cipher = {};
+ function encode (msmCapturado, desplazamiento) {
+  const codigoAssci = msmCapturado.charcodeAt ();
+  const nuevoNumeroAssci = (codigoAssci - 65 + desplazamiento) % 26 + 65;
+  const nuevaLetra = String.fromCharcode(nuevoNumeroAssci)
+  //return nuevaLetra;
+  console.log(nuevaLetra)
+  msmsalida.innerHTML = nuevaLetra;
+
+
+ }
+ */
+  //decode: () => {
+    
+  //}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 /* Acá va tu código */
+const ocultar = () => {
+	const inicio = document.getElementById('inicio');
+	inicio.style.display="none";
+
+	const ventana = document.getElementById('ventana');
+	ventana.style.display="block";
+};
+document.getElementById("myBtn").addEventListener("click", ocultar)
